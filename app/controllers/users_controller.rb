b165e4611015868a3cd30c805d44e8f39ddcb243
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-07-15 12:06:40 +0000

    [Add]3-27(user.show)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    
+  end
+
+  def edit
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
   
-  has_one_attached :image
+  has_one_attached :profileimage
   belongs_to :user
+  
+  validates :title, presence: true
+  validates :body, presence: true
   
 end
