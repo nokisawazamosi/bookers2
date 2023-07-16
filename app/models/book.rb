4278d0d87cfc65467f0d477ed6d832a998582d62
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-07-16 10:45:36 +0000

    [Add]3-27users

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,11 @@
 class Book < ApplicationRecord
   
-  has_one_attached :profileimage
+  has_one_attached :profile_image
   belongs_to :user
   
   validates :title, presence: true
   validates :body, presence: true
+  
+ 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
   
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:new, :show, :edit, :index, :update]
 
 
 end
